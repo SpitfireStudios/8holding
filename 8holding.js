870cc3d833bc9cd687c0283e1659ad2b5b37105a
Toprak Tosun <toprak.tosun@me.com>
2021-02-18 00:32:24 +0300

    commit on main

--- 8holding.js
+++ 8holding.js
@@ -7,10 +7,20 @@ for (i = 0; i < coll.length; i++) {
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
-      content.style.padding = "0em";
     } else {
-      content.style.padding = content.scrollHeight/3 + "px";
-      content.style.maxHeight = content.scrollHeight + "em";
-    } 
+      content.style.maxHeight = content.scrollHeight + content.style.padding + "em";
+    }
+  });
+}
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.padding){
+      content.style.padding = null;
+    } else {
+      content.style.padding = "2em";
+    }
   });
 }
